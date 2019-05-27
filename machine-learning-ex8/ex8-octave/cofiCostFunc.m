cf1fcb2e4b3753cacc838e6b71906817b66acc07
Cameron McCormack <cam.mac@live.co.uk>
2019-05-27 20:37:02 +0100

    ex8 complete

--- machine-learning-ex8/ex8-octave/cofiCostFunc.m
+++ machine-learning-ex8/ex8-octave/cofiCostFunc.m
@@ -40,19 +40,17 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+M = ((Theta * X')' - Y).*R;
 
+% without regularization
+J = 0.5 * sum(sum(M.^2));
+X_grad = M * Theta;
+Theta_grad = M' * X;
 
-
-
-
-
-
-
-
-
-
-
-
+% add regularization
+J = J + (lambda/2) * (sum(sum(Theta.^2)) + sum(sum(X.^2)));
+X_grad = X_grad + lambda*X;
+Theta_grad = Theta_grad + lambda*Theta; 
 
 
 % =============================================================
