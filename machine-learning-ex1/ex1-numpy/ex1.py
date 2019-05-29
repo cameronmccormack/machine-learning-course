3d6fe02b5923eba0416a345460abc2471347d057
Cameron McCormack <cam.mac@live.co.uk>
2019-05-29 12:18:40 +0100

    cost function completed

--- machine-learning-ex1/ex1-numpy/ex1.py
+++ machine-learning-ex1/ex1-numpy/ex1.py
@@ -12,7 +12,7 @@ def warmUpExercise():
 def computeCost(X, y, theta):
     m = np.size(y)
     J = (1/(2*m)) * np.sum((X @ theta - y)**2)
-
+    return J
 
 ## Machine Learning Online Class - Exercise 1: Linear Regression
 
@@ -30,17 +30,16 @@ c = input("Program paused. Press enter to continue.")
 print("Plotting Data ...")
 f = open("ex1data1.txt", "r")
 X, y = np.loadtxt(f, delimiter=",", unpack=True)
+X = np.array([X]).T
+y = np.array([y]).T
 m = np.size(y)
 plt.plot(X, y, 'ro')
-#plt.show()
+plt.show()
 c = input("Program paused. Press enter to continue.")
 
 
 ## Part 3: Cost and Gradient Descent
-
-X = np.append([np.ones(m)], [X], axis=0)
-X = X.transpose()
-y = y.transpose()
+X = np.c_[np.ones(m), X]
 theta = np.zeros((2, 1))
 
 # gradient descent settings
@@ -50,4 +49,9 @@ alpha = 0.01
 print("Testing the cost function")
 J = computeCost(X, y, theta)
 print("With theta = [0 ; 0], Cost computed = {}".format(J))
+print("Expected cost value (approx) 32.07")
 
+# further testing
+J = computeCost(X, y, np.array([[-1], [2]]))
+print("\nWith theta = [-1; 2], Cost computed = {}".format(J))
+print("Expected cost value (approx) 54.24")
