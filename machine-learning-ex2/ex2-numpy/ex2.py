c33c4084e192a39c43d51965276ebcff62a018eb
Cameron McCormack <cam.mac@live.co.uk>
2019-05-31 22:58:05 +0100

    part 1 of ex2 completed

---
+++ machine-learning-ex2/ex2-numpy/ex2.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+def plotData(X, y):
+    pos = []
+    neg = []
+    for i in range(0, np.size(y) - 1):
+        if y[i, 0] == 1:
+            pos.append(i)
+        else:
+            neg.append(i)
+    plt.plot(X[pos, 0], X[pos, 1], 'k+', label="Admitted")
+    plt.plot(X[neg, 0], X[neg, 1], 'ko', label="Not admitted")
+    plt.xlabel("Exam 1 Score")
+    plt.ylabel("Exam 2 Score")
+    plt.legend()
+    plt.show(block=False)
+
+
+if __name__ == "__main__":
+
+    # Exercise 2 - logistic regression
+
+    # load data
+    data = np.loadtxt("data/ex2data1.txt", delimiter=",")
+    X, y = data[:, :2], data[:, 2:]
+
+    print(X)
+    print(y)
+
+    # part 1: plotting
+
+    print("Plotting data with + indicating (y=1) examples and o indicating "
+          "(y=0) examples.")
+    plotData(X, y)
+    c = input("Program paused. Press enter to continue.")
+
+    # part 2: compute cost and gradient
