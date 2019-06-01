0078bcc1da72e2756a699b51fd5ab8c93f7a569b
Cameron McCormack <cam.mac@live.co.uk>
2019-06-01 21:41:28 +0100

    ex2.py complete

--- machine-learning-ex2/ex2-numpy/ex2.py
+++ machine-learning-ex2/ex2-numpy/ex2.py
@@ -8,13 +8,11 @@ import matplotlib.pyplot as plt
 def plotData(X, y):
     pos = []
     neg = []
-
     for i in range(0, np.size(y) - 1):
         if y[i, 0] == 1:
             pos.append(i)
         else:
             neg.append(i)
-
     plt.plot(X[pos, 0], X[pos, 1], 'k+', label="Admitted")
     plt.plot(X[neg, 0], X[neg, 1], 'ko', label="Not admitted")
     plt.xlabel("Exam 1 Score")
@@ -36,6 +34,25 @@ def costFunction(theta, X, y):
                  - (1-y).T @ np.log(1 - sigmoid(X @ theta)))
     grad = (1/m) * X.T @ (sigmoid(X @ theta) - y)
     return np.asscalar(J), grad
+
+
+def plotDecisionBoundary(theta, X, y):
+    plot_x1 = np.array([np.min(X[:, 1])-2, np.max(X[:, 1])+2])
+    plot_x2 = (-1/theta[2, 0]) * (theta[0, 0] + theta[1, 0]*plot_x1)
+    plt.plot(plot_x1, plot_x2, 'k-')
+    plt.show(block=False)
+
+
+def predict(theta, X):
+    m = np.size(X, 0)
+    p = np.zeros((m, 1))
+    hypotheses = sigmoid(X @ theta)
+    for i in range(0, m-1):
+        if hypotheses[i, 0] >= 0.5:
+            p[i, 0] = 1
+        else:
+            p[i, 0] = 0
+    return p
 
 
 if __name__ == "__main__":
@@ -82,8 +99,8 @@ if __name__ == "__main__":
     c = input("\nProgram paused. Press enter to continue.")
 
     # part 3: optimizing
-    opt_result = opt.fmin_tnc(costFunction, initial_theta, args=(X,y))
-    theta = np.reshape(opt_result[0], (3,1))
+    opt_result = opt.fmin_tnc(costFunction, initial_theta, args=(X, y))
+    theta = np.reshape(opt_result[0], (3, 1))
     cost = costFunction(theta, X, y)[0]
 
     # print cost and theta
@@ -96,3 +113,19 @@ if __name__ == "__main__":
     # plot boundary
     plotDecisionBoundary(theta, X, y)
     c = input("\nProgram paused. Press enter to continue.")
+
+    # part 4: predict and accuracies
+
+    # predict probabilty for a student with score 45 on exam one
+    # and 85 on exam two
+    prob = sigmoid(theta.T @ np.array([[1], [45], [85]]))
+    print("For a student with score 45 and 85, we predict an admission "
+          "probability of {}".format(np.asscalar(prob)))
+    print("Expect value: 0.775 +/- 0.002")
+
+    # compute accuracy on our training set
+    p = predict(theta, X)
+    accuracy = np.mean(np.double(p == y)*100)
+    print("Train accuracy: {} %".format(accuracy))
+    print("Expected accuracy (approx): 89.0 %")
+    c = input("\nProgram complete. Press enter to exit.")
