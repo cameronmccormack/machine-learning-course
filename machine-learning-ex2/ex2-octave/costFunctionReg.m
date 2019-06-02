0986b704a0c20f46279c0ef92a55c5652c78665a
Cameron McCormack <cam.mac@live.co.uk>
2019-06-02 16:42:42 +0100

    stopped function printing results

--- machine-learning-ex2/ex2-octave/costFunctionReg.m
+++ machine-learning-ex2/ex2-octave/costFunctionReg.m
@@ -18,8 +18,8 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 J = (1/m) * (-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta))) + (lambda/(2*m)) * (sum(theta.^2)-theta(1)^2);
-grad = (1/m) * X'*(sigmoid(X*theta) - y) + (lambda/m)*theta
-grad(1) = grad(1) - (lambda/m)*theta(1)
+grad = (1/m) * X'*(sigmoid(X*theta) - y) + (lambda/m)*theta;
+grad(1) = grad(1) - (lambda/m)*theta(1);
 
 % =============================================================
 
