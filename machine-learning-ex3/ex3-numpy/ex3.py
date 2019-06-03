45132d59f31dfbcac1f6fcbb0453000ae77668bf
Cameron McCormack <cam.mac@live.co.uk>
2019-06-03 23:04:01 +0100

    ex3.py complete

--- machine-learning-ex3/ex3-numpy/ex3.py
+++ machine-learning-ex3/ex3-numpy/ex3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.io as sio
+import scipy.optimize as opt
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -32,10 +33,62 @@ def displayData(X, example_width=None):
     plt.show(block=False)
 
 
+def sigmoid(z):
+    denom = 1 + np.exp(-z)
+    return denom**-1
+
+
+def lrCostFunction(theta, X, y, reg_lambda):
+    m = np.size(y)
+
+    # optimization functions unroll theta, reshape here
+    n = np.size(theta)
+    theta = np.reshape(theta, (n, 1))
+
+    J = ((1/m) * (-y.T @ np.log(sigmoid(X @ theta))
+         - (1 - y.T) @ np.log(1 - sigmoid(X @ theta)))
+         + (reg_lambda/(2 * m)) * (np.sum(theta**2) - theta[0, 0]**2))
+
+    grad = np.zeros((np.size(theta), 1))
+    grad = (1/m) * X.T @ (sigmoid(X @ theta) - y) + (reg_lambda / m) * theta
+    grad[0, 0] = grad[0, 0] - (reg_lambda / m) * theta[0, 0]
+
+    return np.asscalar(J), grad
+
+
+def oneVsAll(X, y, num_labels, lambda_reg):
+    # add ones to X data matrix
+    m = np.size(X, 0)
+    X = np.c_[np.ones((m, 1)), X]
+
+    # more useful variables
+    n = np.size(X, 1)
+    all_theta = np.zeros((num_labels, n))
+
+    # train logistic regression classifiers
+    for i in range(0, num_labels):
+        initial_theta = np.zeros((n, 1))
+        y_temp = np.where(y == i+1, 1, 0)
+        opt_result = opt.fmin_tnc(lrCostFunction, x0=initial_theta,
+                                  args=(X, y_temp, lambda_reg))
+        all_theta[i, :] = opt_result[0]
+    return all_theta
+
+
+def predictOneVsAll(all_theta, X):
+    m = np.size(X, 0)
+    p = np.zeros((m, 1))
+    X = np.c_[np.ones((m, 1)), X]
+    p = np.argmax(X @ all_theta.T, 1) + 1
+    return np.reshape(p, (m, 1))
+
+
 if __name__ == "__main__":
     # set up parameters
     input_layer_size = 400
     num_labels = 10
+
+    # part 1: loading and visualizing data
 
     # load training data
     print("Loading and visualizing data ...")
@@ -49,3 +102,35 @@ if __name__ == "__main__":
     sel = X[rand_indices[:100], :]
     displayData(sel)
     c = input("\nProgram paused. Press enter to continue.")
+
+    # part 2a: vectorize logistic regression
+
+    # test case for lrCostFunction
+    print("\nTesting lrCostFunction with regularization")
+    theta_t = np.array([[-2], [-1], [1], [2]])
+    X_t = np.c_[np.ones((5, 1)), np.reshape(range(1, 16), (3, 5)).T/10]
+    y_t = np.array([[1], [0], [1], [0], [1]])
+    lambda_t = 3
+    J, grad = lrCostFunction(theta_t, X_t, y_t, lambda_t)
+
+    print("\nCost: {}".format(J))
+    print("Expected cost: 2.534819")
+    print("\nGradients:")
+    print(grad)
+    print("Expected gradients:")
+    print("0.146561\n-0.548558\n0.724722\n1.398003")
+    c = input("\nProgram paused. Press enter to continue.")
+
+    # part 2b: one-vs-all training
+
+    print("\nTraining one-vs-all logistic regression ...")
+    lambda_reg = 0.1
+    all_theta = oneVsAll(X, y, num_labels, lambda_reg)
+    c = input("\nProgram paused. Press enter to continue.")
+
+    # part 3: predict for one-vs-all
+
+    pred = predictOneVsAll(all_theta, X)
+    accuracy = np.mean(np.double(pred == y)*100)
+    print("\n Training set accuracy: {}".format(accuracy))
+    c = input("\nProgram complete. Press enter to exit.")
